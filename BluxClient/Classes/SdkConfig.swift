742dad82c77c6c1338123b2778fad384b6933550
geongun20 <leesg9@snu.ac.kr>
2024-09-04 16:43:25 +0900

    0.2.11

--- BluxClient/Classes/SdkConfig.swift
+++ BluxClient/Classes/SdkConfig.swift
@@ -14,7 +14,7 @@ public enum SdkType: String {
 }
 
 final class SdkConfig {
-    static var sdkVersion = "0.2.10"
+    static var sdkVersion = "0.2.11"
     static var sdkType: SdkType = .native
     
     static var bluxAppGroupNameKey = "BluxAppGroupName"
