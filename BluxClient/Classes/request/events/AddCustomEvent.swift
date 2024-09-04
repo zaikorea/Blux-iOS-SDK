9f55ff45532e9c285abe9350e1cc3b9e6dc4d3b0
geongun20 <leesg9@snu.ac.kr>
2024-09-04 14:02:55 +0900

    [feat] collector v2로 이전

--- BluxClient/Classes/request/events/AddCustomEvent.swift
+++ BluxClient/Classes/request/events/AddCustomEvent.swift
@@ -7,36 +7,19 @@ public class AddCustomEvent: EventRequest {
         self.events.append(
             try Event(eventType: builder.eventType)
                     .setItemId(builder.itemId)
-                    .setTimestamp(builder.timestamp)
                     .setEventValue(builder.eventValue)
-                    .setFrom(builder.from)
-                    .setUrl(builder.url)
-                    .setRef(builder.ref)
-                    .setRecommendationId(builder.recommendationId)
                     .setEventProperties(builder.eventProperties)
-                    .setUserProperties(builder.userProperties)
         )
     }
     
     public class Builder {
         var itemId: String?
-        var timestamp: Double = Utils.getCurrentUnixTimestamp()
         var eventType: String
         var eventValue: String? = nil
-        var from: String? = nil
-        var url: String? = nil
-        var ref: String? = nil
-        var recommendationId: String? = nil
         var eventProperties: [String: String]? = nil
-        var userProperties: [String: String]? = nil
         
         public init(eventType: String) {
             self.eventType = eventType
-        }
-        
-        public func timestamp(_ timestamp: Double) -> Builder {
-            self.timestamp = timestamp
-            return self
         }
         
         public func eventValue(_ eventValue: String) -> Builder {
@@ -44,33 +27,8 @@ public class AddCustomEvent: EventRequest {
             return self
         }
         
-        public func from(_ from: String) -> Builder {
-            self.from = from
-            return self
-        }
-        
-        public func url(_ url: String) -> Builder {
-            self.url = url
-            return self
-        }
-        
-        public func ref(_ ref: String) -> Builder {
-            self.ref = ref
-            return self
-        }
-        
-        public func recommendationId(_ recommendationId: String) -> Builder {
-            self.recommendationId = recommendationId
-            return self
-        }
-        
         public func eventProperties(_ eventProperties: [String: String]) -> Builder {
             self.eventProperties = eventProperties
-            return self
-        }
-        
-        public func userProperties(_ userProperties: [String: String]) -> Builder {
-            self.userProperties = userProperties
             return self
         }
         
--- BluxClient/Classes/request/events/AddLikeEvent.swift
+++ BluxClient/Classes/request/events/AddLikeEvent.swift
@@ -8,70 +8,27 @@ public class AddLikeEvent: EventRequest {
         self.events.append(
             try Event(eventType: builder.eventType)
                 .setItemId(builder.itemId)
-                .setTimestamp(builder.timestamp)
                 .setEventValue(builder.eventValue)
-                .setFrom(builder.from)
-                .setUrl(builder.url)
-                .setRef(builder.ref)
-                .setRecommendationId(builder.recommendationId)
                 .setEventProperties(builder.eventProperties)
-                .setUserProperties(builder.userProperties)
         )
     }
     
     public class Builder {
         var itemId: String
-        var timestamp: Double = Utils.getCurrentUnixTimestamp()
         var eventType: String = AddLikeEvent.DEFAULT_EVENT_TYPE
         var eventValue: String? = nil
-        var from: String? = nil
-        var url: String? = nil
-        var ref: String? = nil
-        var recommendationId: String? = nil
         var eventProperties: [String: String]? = nil
-        var userProperties: [String: String]? = nil
         
         public init(itemId: String) {
             self.itemId = itemId
-        }
-        
-        public func timestamp(_ timestamp: Double) -> Builder {
-            self.timestamp = timestamp
-            return self
         }
         
         public func eventValue(_ eventValue: String) -> Builder {
             self.eventValue = eventValue
             return self
         }
-        
-        public func from(_ from: String) -> Builder {
-            self.from = from
-            return self
-        }
-        
-        public func url(_ url: String) -> Builder {
-            self.url = url
-            return self
-        }
-        
-        public func ref(_ ref: String) -> Builder {
-            self.ref = ref
-            return self
-        }
-        
-        public func recommendationId(_ recommendationId: String) -> Builder {
-            self.recommendationId = recommendationId
-            return self
-        }
-        
         public func eventProperties(_ eventProperties: [String: String]) -> Builder {
             self.eventProperties = eventProperties
-            return self
-        }
-        
-        public func userProperties(_ userProperties: [String: String]) -> Builder {
-            self.userProperties = userProperties
             return self
         }
         
