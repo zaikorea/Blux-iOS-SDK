7fa647b738d84c7a680379cc4a065f60a51543fd
NoMoreViolence <ljh86029926@gmail.com>
2024-11-12 17:41:14 +0900

    feat: add event queue for guaranteed order of requests

---
+++ BluxClient/Classes/EventQueue.swift
@@ -0,0 +1,42 @@
+//
+//  EventQueue.swift
+//  BluxClient
+//
+//  Created by jihoon on 11/12/24.
+//
+import Foundation
+
+import Foundation
+
+class EventQueue {
+    static let shared = EventQueue()
+    
+    private var eventsQueue: [() -> Void] = []
+    private var isProcessing = false
+    private let queue = DispatchQueue(label: "eventQueue")
+
+    private init() {}
+
+    func addEvent(_ eventTask: @escaping () -> Void) {
+        queue.async { [weak self] in
+            self?.eventsQueue.append(eventTask)
+            self?.processNext()
+        }
+    }
+
+    private func processNext() {
+        queue.async { [weak self] in
+            guard let self = self, !self.isProcessing, let nextTask = self.eventsQueue.first else { return }
+            
+            self.isProcessing = true
+            
+            nextTask()
+            
+            self.queue.async {
+                self.eventsQueue.removeFirst()
+                self.isProcessing = false
+                self.processNext()
+            }
+        }
+    }
+}
