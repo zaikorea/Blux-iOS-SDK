3932dcb559038b1d45696bd50f9c34cf7185603d
kyungwon-lee <kw375310@gmail.com>
2025-06-20 02:26:47 +0900

    0.4.4

--- BluxClient.podspec
+++ BluxClient.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'BluxClient'
-  s.version          = '0.4.3'
+  s.version          = '0.4.4'
   s.summary          = 'Blux iOS SDK.'
 
   s.homepage         = 'https://github.com/zaikorea/Blux-iOS-SDK.git'
--- BluxClient/Classes/SdkConfig.swift
+++ BluxClient/Classes/SdkConfig.swift
@@ -14,7 +14,7 @@ public enum SdkType: String {
 }
 
 final class SdkConfig {
-    static var sdkVersion = "0.4.3"
+    static var sdkVersion = "0.4.4"
     static var sdkType: SdkType = .native
     
     static var bluxAppGroupNameKey = "BluxAppGroupName"
