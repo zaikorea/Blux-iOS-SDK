c194ca44d19af226862f04af533cd56071d5a5f2
silvermanseoul <silverman.unicorn@gmail.com>
2025-11-18 20:41:27 +0900

    feat: BluxNotificationServiceExtensionHelper.didReceive에 replaceContent 파라미터 추가

--- BluxClient/Classes/BluxNotificationServiceExtension.swift
+++ BluxClient/Classes/BluxNotificationServiceExtension.swift
@@ -14,7 +14,7 @@ open class BluxNotificationServiceExtension: UNNotificationServiceExtension {
     override open func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         BluxNotificationServiceExtensionHelper.shared.didReceive(request, withContentHandler: contentHandler)
     }
-    
+
     override open func serviceExtensionTimeWillExpire() {
         BluxNotificationServiceExtensionHelper.shared.serviceExtensionTimeWillExpire()
     }
@@ -22,60 +22,72 @@ open class BluxNotificationServiceExtension: UNNotificationServiceExtension {
 
 @objc public class BluxNotificationServiceExtensionHelper: NSObject {
     @objc public static let shared = BluxNotificationServiceExtensionHelper()
-    
+
     // A closure for delivering mutated notification content to the system
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
-    
-    @objc public func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-        self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
-        if let bestAttemptContent = bestAttemptContent {
-            guard let bluxNotification = BluxNotification.getBluxNotificationFromUNNotificationContent(request.content) else {
-                contentHandler(bestAttemptContent)
-                return
-            }
-            
-            EventService.createReceived(bluxNotification.id)
 
-            guard let imageUrl = bluxNotification.imageUrl,
-                  let attachmentUrl = URL(string: imageUrl)
-            else {
+    @objc public func didReceive(_ request: UNNotificationRequest,
+                                 withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void,
+                                 replaceContent: UNMutableNotificationContent? = nil)
+    {
+        self.contentHandler = contentHandler
+        bestAttemptContent = replaceContent ?? (request.content.mutableCopy() as? UNMutableNotificationContent)
+
+        guard let bestAttemptContent = bestAttemptContent else {
+            contentHandler(request.content)
+            return
+        }
+
+        guard let bluxNotification = BluxNotification.getBluxNotificationFromUNNotificationContent(request.content) else {
+            contentHandler(bestAttemptContent)
+            return
+        }
+
+        EventService.createReceived(bluxNotification.id)
+
+        guard let imageUrl = bluxNotification.imageUrl,
+              let attachmentUrl = URL(string: imageUrl)
+        else {
+            contentHandler(bestAttemptContent)
+            return
+        }
+
+        // Download image from imageUrl and attach to the notification
+        let task = URLSession.shared.downloadTask(with: attachmentUrl) { downloadedUrl, _, error in
+            if let _ = error {
                 contentHandler(bestAttemptContent)
                 return
             }
-            
-            // Download image from imageUrl and attach to the notification
-            let task = URLSession.shared.downloadTask(with: attachmentUrl) { downloadedUrl, _, error in
-                if let _ = error {
-                    contentHandler(bestAttemptContent)
-                    return
-                }
-                
-                if let downloadedUrl = downloadedUrl, let attachment = try? UNNotificationAttachment(identifier: "Blux_notification_attachment", url: downloadedUrl, options: [UNNotificationAttachmentOptionsTypeHintKey: kUTTypePNG]) {
-                    bestAttemptContent.attachments = [attachment]
-                }
-                
-                contentHandler(bestAttemptContent)
+
+            if let downloadedUrl = downloadedUrl,
+               let attachment = try? UNNotificationAttachment(
+                   identifier: "Blux_notification_attachment",
+                   url: downloadedUrl,
+                   options: [UNNotificationAttachmentOptionsTypeHintKey: kUTTypePNG]
+               )
+            {
+                bestAttemptContent.attachments = [attachment]
             }
-            
-            task.resume()
+
+            contentHandler(bestAttemptContent)
         }
+
+        task.resume()
     }
-    
+
     // Called just before when the extension is terminated by the system
     @objc public func serviceExtensionTimeWillExpire() {
         if let contentHandler = contentHandler, let bestAttemptContent = bestAttemptContent {
             contentHandler(bestAttemptContent)
         }
     }
-    
+
     @objc public func isBluxNotification(_ request: UNNotificationRequest) -> Bool {
         if let _ = BluxNotification.getBluxNotificationFromUNNotificationContent(request.content) {
             return true
         }
-        
+
         return false
     }
 }
