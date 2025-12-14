c70df48217b15d67c092e275ec75a1897e895f81
silvermanseoul <silverman.unicorn@gmail.com>
2025-12-15 03:38:01 +0900

    refactor: foregroundReceived -> foregroundWillDisplay 이름 변경

--- BluxClient/Classes/EventHandlers.swift
+++ BluxClient/Classes/EventHandlers.swift
@@ -1,14 +1,8 @@
-//
-//  EventHandlers.swift
-//  BluxClient
-//
-//  Created by Tommy on 5/30/24.
-//
-
 import Foundation
 
 enum EventHandlers {
     static var unhandledNotification: BluxNotification?
+    static var notificationForegroundWillDisplay: ((NotificationReceivedEvent) -> Void)?
     static var notificationClicked: ((BluxNotification) -> Void)?
-    static var notificationForegroundReceived: ((NotificationReceivedEvent) -> Void)?
+    
 }
