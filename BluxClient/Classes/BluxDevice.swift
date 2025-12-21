9a02d6dd4b597698ed4fda6d5d9b3c44ca72281a
geongun20 <leesg9@snu.ac.kr>
2025-12-21 13:26:50 +0900

    chore: isVisitHandlingInSdk 필드 제거 (과거 하위호환성 위한 필드)

--- BluxClient/Classes/BluxDevice.swift
+++ BluxClient/Classes/BluxDevice.swift
@@ -40,7 +40,6 @@ open class BluxDeviceInfo: Codable {
     public var languageCode: String?
     public var countryCode: String?
     public var sdkType: String
-    public var isVisitHandlingInSdk: Bool = true
 
     enum CodingKeys: String,
         CodingKey
@@ -57,7 +56,6 @@ open class BluxDeviceInfo: Codable {
         case languageCode = "language_code"
         case countryCode = "country_code"
         case sdkType = "sdk_type"
-        case isVisitHandlingInSdk
     }
 
     public init(
@@ -105,7 +103,6 @@ open class BluxDeviceInfo: Codable {
         try container.encode(languageCode, forKey: .languageCode)
         try container.encode(countryCode, forKey: .countryCode)
         try container.encode(sdkType, forKey: .sdkType)
-        try container.encode(isVisitHandlingInSdk, forKey: .isVisitHandlingInSdk)
     }
 }
 
