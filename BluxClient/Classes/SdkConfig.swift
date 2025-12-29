0912ca32689b5f3aa17e666dcee43780bdb6176f
NoMoreViolence <ljh86029926@gmail.com>
2025-12-30 00:43:35 +0900

    0.6.4

--- BluxClient/Classes/SdkConfig.swift
+++ BluxClient/Classes/SdkConfig.swift
@@ -14,7 +14,7 @@ public enum SdkType: String {
 }
 
 enum SdkConfig {
-    static var sdkVersion = "0.6.3"
+    static var sdkVersion = "0.6.4"
     static var sdkType: SdkType = .native
 
     static var bluxAppGroupNameKey = "BluxAppGroupName"
