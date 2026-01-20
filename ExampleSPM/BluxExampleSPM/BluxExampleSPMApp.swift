6b48f34c84680582df91cad1f91048938667c790
silvermanseoul <silverman.unicorn@gmail.com>
2026-01-20 13:59:43 +0900

    feat(example): BluxExampleSPM 프로젝트 추가

---
+++ ExampleSPM/BluxExampleSPM/BluxExampleSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluxExampleSPMApp.swift
+//  BluxExampleSPM
+//
+//  Created by 박은만 on 1/20/26.
+//
+
+import SwiftUI
+
+@main
+struct BluxExampleSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
