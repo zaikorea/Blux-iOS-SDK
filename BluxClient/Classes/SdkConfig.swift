fd8bc42cf5e4e7d901cae8121b010780989f82e8
silvermanseoul <silverman.unicorn@gmail.com>
2026-01-20 17:05:44 +0900

    0.6.7

--- BluxClient/Classes/SdkConfig.swift
+++ BluxClient/Classes/SdkConfig.swift
@@ -14,7 +14,7 @@ public enum SdkType: String {
 }
 
 enum SdkConfig {
-    static var sdkVersion = "0.6.6"
+    static var sdkVersion = "0.6.7"
     static var sdkType: SdkType = .native
 
     static var bluxAppGroupNameKey = "BluxAppGroupName"
